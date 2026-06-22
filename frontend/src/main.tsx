5bd72e2a939ded5f7d109e6f8cf9768ad37ead99
ilhmpbta <178406185+ilhmpbta@users.noreply.github.com>
2026-06-22 20:01:00 +0700

    refactor: use modern main.tsx approach

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,29 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './styles/globals.css'
-import App from './App.tsx'
+
+import Landing from './pages/Landing.tsx'
+import Login from './pages/Login.tsx'
+import Register from './pages/Register.tsx'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Landing />,
+  },
+  {
+    path: '/login',
+    element: <Login />,
+  },
+  {
+    path: '/register',
+    element: <Register />,
+  },
+])
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
